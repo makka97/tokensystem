b9d8496be87aa69057efdad5f02af543d63a2f75
Gaglani <dgaglani@hq.corp.viasat.com>
2017-05-25 15:32:36 +0530

    Included barcode reading changes

--- TokenSystemClientPythonScript.py
+++ TokenSystemClientPythonScript.py
@@ -3,13 +3,17 @@ import requests, sys, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="Base url for tokens server")
-parser.add_argument("barcode", help="Barcode used for item purchase")
 args = parser.parse_args()
 
-purchasePostRes = requests.post(args.url, data = json.dumps({'barcodeNumber':args.barcode, 'vendorName':'V1'}))
+while True:
+    inp = raw_input()
+    if inp == "":
+        continue
+    else:
+        purchasePostRes = requests.post(args.url, data = json.dumps({'barcodeNumber':inp, 'vendorName':'V1'}))
 
-if purchasePostRes.status_code == 200:
-    tokensCountRes = requests.get(args.url+'?vendorName=V1')
-    print(tokensCountRes.text)
-else:
-    print("Error from Server: Invalid barcode : " + args.barcode)
+        if purchasePostRes.status_code == 200:
+            tokensCountRes = requests.get(args.url+'?vendorName=V1')
+            print(tokensCountRes.text)
+        else:
+            print("Error from Server: Invalid barcode : " + inp)
