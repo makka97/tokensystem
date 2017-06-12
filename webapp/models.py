39760e531e2fd47710eb69355e3728043fbdde99
Gaglani <dgaglani@hq.corp.viasat.com>
2017-06-12 12:49:23 +0530

    Added a model manager to TokenModel for admin view.

--- webapp/models.py
+++ webapp/models.py
@@ -1,9 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db import connection
 
 
 # Create your models here.
+class TokenManager(models.Manager):
+    def get_token_count_by_date(self):
+        #Create Query
+        cursor = connection.cursor()
+        cursor.execute("""select t.vendor_id, date(t.tokenDateTime), count(*) from webapp_token t group by t.vendor_id, date(t.tokenDateTime)""")
+
+        #Execute Query
+        result_list = []
+        for tokenDets in cursor.fetchall():
+            #Create model instance
+            v = Vendor.objects.get(id = tokenDets[0])
+            tokenDet = self.model(vendor = v, tokenDateTime = tokenDets[1])
+
+            #Add new attribute to model
+            tokenDet.num_count = tokenDets[2]
+
+            result_list.append(tokenDet)
+        
+        #Return model instances
+        return result_list
+
 class Vendor(models.Model):
     vendorName = models.CharField(max_length=100)
 
@@ -22,6 +44,9 @@ class Token(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     barcode = models.ForeignKey(Barcode, on_delete=models.CASCADE)
     tokenDateTime = models.DateTimeField()
+
+    objects = models.Manager()
+    token_count_objects = TokenManager()
 
 
 
